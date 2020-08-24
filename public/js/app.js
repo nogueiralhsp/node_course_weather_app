5ff21370ab0c6ba0b55cb12c7358874b05038d89
henrique <nogueiralhsp@gmail.com>
2020-08-24 16:24:37 +0100

    text misspelling

--- public/js/app.js
+++ public/js/app.js
@@ -7,14 +7,16 @@ const search = document.querySelector('input')
 
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
+const messageThree = document.querySelector('#message-3')
 
 weatherForm.addEventListener('submit',(e)=>{
      e.preventDefault()
 
      const location = search.value
 
-     messageOne.textContent='Loadig...'
+     messageOne.textContent='Loading...'
      messageTwo.textContent=''
+     messageThree.textContent=''
 
      fetch (url+location).then((response)=>{
           response.json().then((data)=>{
